466cb4f9f870f1cb23fd4b9f2084a15227b61f1c
decki-id <d.herdiawan.s@gmail.com>
2025-08-10 21:52:12 +0700

    Edit the project path comments

--- public/js/app.js
+++ public/js/app.js
@@ -50035,8 +50035,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\My Projects\htdocs\deckiherdiawans.com\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\My Projects\htdocs\deckiherdiawans.com\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\repo\php\instadeck\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\repo\php\instadeck\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
