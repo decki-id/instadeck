ac02a2cfeb7107cddf034724ab7cca5012eccd82
decki-id <d.herdiawan.s@gmail.com>
2025-08-12 21:30:25 +0700

    Add scroll onload

--- public/js/instadeck.js
+++ public/js/instadeck.js
@@ -1,18 +1,16 @@
+window.onload = function() { window.scrollTo(0, 0) }
+
+
 // Search Bar
 
 document.addEventListener("keyup", function (event) {
-    if (event.which == 191) {
-        document.querySelector("#desktop-search").focus();
-    }
-    
+    if (event.which == 191) { document.querySelector("#desktop-search").focus(); }
     if (event.which == 191 && document.querySelector(".dhs_search-bar-responsive").classList.contains("show")) {
         document.querySelector("#mobile-search").focus();
     }
 })
 
-function searchToggle() {
-    document.querySelector(".dhs_search-bar-responsive").classList.toggle("show");
-}
+function searchToggle() { document.querySelector(".dhs_search-bar-responsive").classList.toggle("show"); }
 
 
 // Autofocus
